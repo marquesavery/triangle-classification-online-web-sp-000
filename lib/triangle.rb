49e5940c1254fe7ae723d42bf13e98df9d8d0377
Marques Avery <averymarques4@gmail.com>
2020-08-31 19:43:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,8 @@
 class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
 
-  def initialize(attributes)
-  attributes.each {|key, value| self.send(("#{key}="), value)}
+  def initialize()
+    
   end
 
   def kind(key)
