f894ab7bc3dc3f5b5df76c501c395b61366c8297
Marques Avery <averymarques4@gmail.com>
2020-08-31 20:12:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,9 @@ class Triangle
   end
 
   def kind
-    if @side1 == @side2 && @side1 == @side3 && @side2 == @side3
+    if @side1 == 0 && @side2 == 0 && @side3 == 0
+      raise TriangleError
+    elsif @side1 == @side2 && @side1 == @side3 && @side2 == @side3
       self.kind = :equilateral
     elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
       self.kind = :isosceles
