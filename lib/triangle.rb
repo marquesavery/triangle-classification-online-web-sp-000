87a85687eac2944934079cd9349a325db6a3d3d8
Marques Avery <averymarques4@gmail.com>
2020-08-31 20:09:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,8 @@ class Triangle
 
   def kind
     if @side1 == @side2 && @side2 == @side3
+      self.kind = :equilateral
+    elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
   end
 
   class TriangleError < StandardError
