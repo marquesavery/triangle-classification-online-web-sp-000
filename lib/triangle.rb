ae6e301a574bc55b018d3aa8a2da0e7ab044c232
Marques Avery <averymarques4@gmail.com>
2020-08-31 20:06:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,10 @@
 class Triangle
-  attr_accessor :equilateral, :isosceles, :scalene
+  attr_accessor :side1, :side2, :side3
 
   def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
 
   end
 
