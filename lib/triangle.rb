cc3bfd11f5ee78b17809273df7850f673dd94833
Marques Avery <averymarques4@gmail.com>
2020-08-31 20:10:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
     elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
       self.kind = :isosceles
     elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
-
+      self.kind = :scalene
   end
 
   class TriangleError < StandardError
